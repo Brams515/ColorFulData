ec6d306b4bae1b00dfa5a14d98f850749616bbe0
Brams515 <bram.samyn@outlook.com>
2026-09-02 00:26:11 +0200

    updated requirements and main header

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ Script Description:
 The Main script is the main executable for the application, and couples
 the GUI with the underlying logic stored in other scripts.
 ============================================
+Copyright (C) 2026 Bram Samyn
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+============================================
 """
 #===PyQt6 Imports===
 import os
